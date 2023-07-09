dc9bdbf6bd41418ce2d28283cede64a4b35d1987
Mirza Abdullah Baig <abaig7921gmail.com>
2023-07-09 11:58:18 +0500

    Task 13 Done

--- Task 13/Task 13.js
+++ Task 13/Task 13.js
@@ -1,2 +1,16 @@
 // Task 13: Write a program to take a number in a variable, do the required arithmetic to display the following result in your browser:
 
+var a = 10;
+document.write("The Value of a is: " + a + "<br/> <br/>" );
+
+document.write("The Value of  ++a  is: " + ++a + "<br/>");
+document.write("Now the Value of a is: " + a + "<br/><br/>");
+
+document.write("The Value of a++ is: " + a++ + "<br/>");
+document.write("Now the Value of a is: " + a + "<br/><br/>");
+
+document.write("The Value of --a is: " + --a + "<br/>");
+document.write("Now the Value of a is: " + a + "<br/><br/>");
+
+document.write("The Value of a-- is: " + a-- + "<br/>");
+document.write("Now the Value of a is: " + a + "<br/><br/>");
