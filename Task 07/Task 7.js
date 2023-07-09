3e302fa2b46cf61f2925c8e1d5068f3e46df636e
Mirza Abdullah Baig <abaig7921gmail.com>
2023-07-09 12:43:56 +0500

    Task 07 Done

---
+++ Task 07/Task 7.js
@@ -0,0 +1,12 @@
+// Task 07: Display this in your browser
+
+var a = "Variable names can only contain numbers, $ and _ . For example : $my_1stVariable";
+var b = "Variables must begin with a letter, $ or _ . For example : $name, _name or name";
+var c = "Variable names are case sensitive";
+var d = "Variable names should not be JS keywords";
+
+
+document.write(a + "<br/>");
+document.write(b + "<br/>");
+document.write(c + "<br/>");
+document.write(d + "<br/>");
