1f82d9b1da71bbb61af54f04ad892ae159f4aec6
Mirza Abdullah Baig <abaig7921gmail.com>
2023-07-09 12:19:49 +0500

    Task 14 Done

---
+++ Task 14/Task 14.js
@@ -0,0 +1,9 @@
+// Task 14: What will be the output in variables a, b & result after execution of the following script:
+
+var a = 2;
+var b = 1;
+var result = --a - --b + ++b + b--;
+
+document.write("a is " + a + "<br>");
+document.write("b is " + b + "<br>");
+document.write("result is " + result + "<br>");
